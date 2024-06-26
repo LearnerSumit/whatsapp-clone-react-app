def481b31da2eb41dff61189f76357b269fcb822
LearnerSumit <sumit.202104177@tulas.edu.in>
2024-06-26 18:48:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+function App() {
+  return (
+    <Container>
+      <h1>React App</h1>
+      <h1>React App</h1>
+      <h1>React App</h1>
+
+    </Container>
+  )
+}
+
+export default App
+
+const Container = styled.div`
+  background: #f8f9fb;
+  height: 100vh;
+  width: 100%;
+`
